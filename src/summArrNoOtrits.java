ef64be26fbfb00b37d434bfa477cbc5b9dd68be2
xdisa <88941871+xdisa@users.noreply.github.com>
2021-10-27 20:56:01 +0400

    Update summArrNoOtrits.java

--- src/summArrNoOtrits.java
+++ src/summArrNoOtrits.java
@@ -6,21 +6,27 @@ public class summArrNoOtrits {
         int[] array = {1, 1, 1, -27};
         List<Integer> negative = new ArrayList<>();
         List<Integer> positive = new ArrayList<>();
+        if (array.length==0) {
+            System.out.println("massiv pustoy");
+        }
+        else{
 
-        for(int i = 0;i<array.length;i++){
-            if(array[i]<0){
-                negative.add(array[i]); // Add negative number
-            }else{
-                positive.add(array[i]); // Add positive number
+            for(int i = 0;i<array.length;i++){
+                if(array[i]<0){
+                    negative.add(array[i]); // Add negative number
+                }else{
+                    positive.add(array[i]); // Add positive number
+                }
             }
-        }
-        int sum =0;
+            int sum =0;
 
-        for(int i = 0; i<positive.size(); i++){
-            sum+=positive.get(i);
+            for(int i = 0; i<positive.size(); i++){
+                sum+=positive.get(i);
 
-        }
-        System.out.println(sum);
+            }
+            System.out.println(sum);
         }
     }
+    }
+
 
